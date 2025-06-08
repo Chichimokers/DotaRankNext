10255330cebd7cb7345cc9f9b19555e093576f76
Chichimokres <developer1575@gmail.com>
2025-06-08 18:33:04 -0400

    a

--- front/next.config.ts
+++ front/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/rankdota',
-
+  trailingSlash: true,
    images: {
     domains: ['avatars.steamstatic.com'],
   },
