31b7910230169de148dc3ca290905d8cc15d747b
Chichimokres <developer1575@gmail.com>
2025-06-08 22:51:43 -0400

    ddss

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here 
   basePath: '/ranking',
-  assetPrefix:'/ranking/',
+  assetPrefix:'/ranking/',*/
   trailingSlash: true,
    images: {
     domains: ['avatars.steamstatic.com',"esaki-jrr.com"],
